9bff15c3b4f7e3ce3d74d22fcf9d969c6027f942
PierreLgol <plgol.perso@gmail.com>
2026-04-15 23:32:09 +0200

    basic build step, and file structure shape

--- build.zig
+++ build.zig
@@ -1,5 +1,32 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const backend_module = b.createModule(.{
+        .root_source_file = b.path("src/backend/root.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const frontend_module = b.createModule(.{
+        .root_source_file = b.path("src/frontend/root.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const application = b.addExecutable(.{
+        .name = "aimux",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = optimize,
+            .target = target,
+            .imports = &.{
+                .{ .name = "frontend", .module = frontend_module },
+                .{ .name = "backend", .module = backend_module },
+            },
+        }),
+    });
+    b.installArtifact(application);
 }
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub fn main(init: std.process.Init) !void {
+    _ = init;
+    std.debug.print("Hello World!\n", .{});
+}
